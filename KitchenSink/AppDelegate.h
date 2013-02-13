e9a145dba24672be5afae46174986989480e6725
Aaron Rosenbaum <aaronrosenbaum@Aarons-MacBook-Air.local>
2013-02-12 23:51:07 -0500

    Initial Commit

---
+++ KitchenSink/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KitchenSink
+//
+//  Created by Aaron Rosenbaum on 2/12/13.
+//  Copyright (c) 2013 Aaron Rosenbaum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
